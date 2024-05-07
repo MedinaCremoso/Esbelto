f3c28cda982abddf81ffc7815eb76360c8ac7b00
MedinaCremoso <129951266+MedinaCremoso@users.noreply.github.com>
2024-05-07 09:32:06 -0300

    Create Tabuada2.js

---
+++ Tabuada2.js
@@ -0,0 +1,38 @@
+let T = 64; 
+
+console.log(T + "x 1 = ", T * 1);
+console.log(T + "x 2 = ", T * 2);
+console.log(T + "x 3 = ", T * 3);
+console.log(T + "x 4 = ", T * 4);
+console.log(T + "x 5 = ", T * 5);
+console.log(T + "x 6 = ", T * 6);
+console.log(T + "x 7 = ", T * 7);
+console.log(T + "x 8 = ", T * 8);
+console.log(T + "x 9 = ", T * 9);
+console.log(T + "x 10 = ",T * 10);
+
+T=T+1;
+
+console.log(T + "x 1 = ", T * 1);
+console.log(T + "x 2 = ", T * 2);
+console.log(T + "x 3 = ", T * 3);
+console.log(T + "x 4 = ", T * 4);
+console.log(T + "x 5 = ", T * 5);
+console.log(T + "x 6 = ", T * 6);
+console.log(T + "x 7 = ", T * 7);
+console.log(T + "x 8 = ", T * 8);
+console.log(T + "x 9 = ", T * 9);
+console.log(T + "x 10 = ",T * 10);
+
+T++;
+
+console.log(T + "x 1 = ", T * 1);
+console.log(T + "x 2 = ", T * 2);
+console.log(T + "x 3 = ", T * 3);
+console.log(T + "x 4 = ", T * 4);
+console.log(T + "x 5 = ", T * 5);
+console.log(T + "x 6 = ", T * 6);
+console.log(T + "x 7 = ", T * 7);
+console.log(T + "x 8 = ", T * 8);
+console.log(T + "x 9 = ", T * 9);
+console.log(T + "x 10 = ",T * 10);
