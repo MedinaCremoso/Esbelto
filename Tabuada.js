b3278688667cf8e745d06405bc70832a6d29a42d
MedinaCremoso <129951266+MedinaCremoso@users.noreply.github.com>
2024-05-07 09:31:29 -0300

    Create Tabuada.js

---
+++ Tabuada.js
@@ -0,0 +1,40 @@
+console.log("Tabuada do 1")
+
+console.log("1 x 1 = 1");
+console.log("1 x 2 = 2");
+console.log("1 x 3 = 3");
+console.log("1 x 4 = 4");
+console.log("1 x 5 = 5");
+console.log("1 x 6 = 6");
+console.log("1 x 7 = 7");
+console.log("1 x 8 = 8");
+console.log("1 x 9 = 9");
+console.log("1 x 10 = 10");
+
+
+console.log("Tabuada do 3")
+
+console.log("3 x 1 = 3");
+console.log("3 x 2 = 6");
+console.log("3 x 3 = 9");
+console.log("3 x 4 = 12");
+console.log("3 x 5 = 15");
+console.log("3 x 6 = 18");
+console.log("3 x 7 = 21");
+console.log("3 x 8 = 24");
+console.log("3 x 9 = 27");
+console.log("3 x 10 = 30");
+
+
+console.log("Tabuada do 7")
+
+console.log("7 x 1 = 7");
+console.log("7 x 2 = 14");
+console.log("7 x 3 = 21");
+console.log("7 x 4 = 28");
+console.log("7 x 5 = 35");
+console.log("7 x 6 = 42");
+console.log("7 x 7 = 49");
+console.log("7 x 8 = 56");
+console.log("7 x 9 = 63");
+console.log("7 x 10 = 70");
