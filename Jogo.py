092e496fbecc5fe071fc66f1232b293f0df3a55d
MedinaCremoso <129951266+MedinaCremoso@users.noreply.github.com>
2024-05-08 12:28:40 -0300

    Update Jogo.py

--- Jogo.py
+++ Jogo.py
@@ -1,19 +1,19 @@
 import random
-continuar = "S"
+continuar  = "S"
 
-while continuar == "S" :
+while continuar.upper() == "S" :
+    ns = random.randint(1,10)
 
-ns = random.randint(1,10)
+    t = 3
 
-t = 3
+    while( t > 0):
+        print(" voce tem" , t, "temtativa")
+        t = t -1
 
-while(t > 0):
-    print("você tem", t, "Tentativa")
-    t = t - 1
-    nc = int(input ("Digite um Número de 1 a 10: "))
-    if (ns == nc):
-      print("você acertou.")
-    else:
-      print("você errou.")
+        nc = int(input ("Digite um Número de 1 a 10: "))
+        if (ns == nc):
+            print("você acertou.")
+        else:
+            print("você errou.")
 
-      continuar = input("Deseja Continuar (S)im")
+continuar = input("deseja continuar (S)im")
